1c2b61c8e9a78c9b5e517efa946fb5a06c824cbe
onebarloop <onebarloop@posteo.de>
2025-10-17 09:32:44 +0200

    Add mobile sidebar trigger

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
 import type { Metadata } from 'next';
+import { cookies } from 'next/headers';
 import './globals.css';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/custom/sidebar';
 import { Toaster } from '@/components/ui/sonner';
+import  SidebarTrigger  from '@/components/custom/sidebar-trigger';
 
 export const metadata: Metadata = {
   title: 'Protocols',
   description: 'Collaborative tool for generating and managing protocols.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar_state_protocols')?.value === 'true';
   return (
     <html lang="en" className="dark">
       <body className="">
-        <SidebarProvider>
+        <SidebarProvider defaultOpen={defaultOpen}>
+        <SidebarTrigger />
           <AppSidebar />
           <main className="w-full overflow-hidden">{children}</main>
           <Toaster richColors position="top-right" />
---
+++ components/custom/sidebar-trigger.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { useIsMobile } from '@/hooks/use-mobile';
+
+export default function SidebarTriggerMobile() {
+  const isMobile = useIsMobile();
+
+  if (!isMobile) {
+    return null;
+  }
+
+  return <SidebarTrigger className="fixed bottom-2 left-2"/>;
+}
