194c58e56899e7937351029cfb3c99cee4f7dc69
onebarloop <onebarloop@posteo.de>
2025-10-20 21:34:05 +0200

    Remove wysiwyg and dotenv packages

--- db/index.ts
+++ db/index.ts
@@ -1,20 +1,7 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import fs from 'fs';
 import * as schema1 from './schema/protocols';
 import * as schema2 from './schema/auth-schema';
-
-const localPath = '.env.local';
-const prodPath = '.env';
-const envPath =
-  process.env.NODE_ENV === 'production'
-    ? prodPath
-    : fs.existsSync(localPath)
-      ? localPath
-      : prodPath;
-
-config({ path: envPath });
 
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle({ client, schema: { ...schema1, ...schema2 } });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,4 @@
-import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-import fs from 'fs';
-
-const localPath = '.env.local';
-const prodPath = '.env';
-const envPath =
-  process.env.NODE_ENV === 'production'
-    ? prodPath
-    : fs.existsSync(localPath)
-      ? localPath
-      : prodPath;
-
-config({ path: envPath });
 
 export default defineConfig({
   schema: './db/schema',
