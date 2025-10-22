6374d7d742fb6700c58cb2bbf51d1ece050e8cc1
onebarloop <onebarloop@posteo.de>
2025-10-22 09:43:31 +0200

    Add indexes to db schema

---
+++ supabase/migrations/0011_brown_the_spike.sql
@@ -0,0 +1,5 @@
+CREATE INDEX "account_user_id_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "session_user_id_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "session_token_idx" ON "session" USING btree ("token");--> statement-breakpoint
+CREATE INDEX "user_email_idx" ON "user" USING btree ("email");--> statement-breakpoint
+CREATE INDEX "verification_identifier_idx" ON "verification" USING btree ("identifier");
