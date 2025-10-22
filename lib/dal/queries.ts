3dbcf6b47f0d98b75744348762ca4e07c8f0101a
onebarloop <onebarloop@posteo.de>
2025-10-22 10:45:57 +0200

    Error handling in db query

--- lib/dal/queries.ts
+++ lib/dal/queries.ts
@@ -22,22 +22,27 @@ export type ProtocolNavItem = Awaited<
   ReturnType<typeof getAllProtocols>
 >[number];
 
-export function getProtocolById(id: string) {
-  return unstable_cache(
-    () => {
-      return db.query.protocols.findFirst({
-        where: (protocols, { eq }) => eq(protocols.id, id),
-        with: {
-          author: true,
-          editor: true,
-        },
-      });
-    },
-    ['protocol', id],
-    {
-      tags: [`protocol-${id}`],
-    },
-  )();
+export async function getProtocolById(id: string) {
+  try {
+    return await unstable_cache(
+      async () => {
+        return await db.query.protocols.findFirst({
+          where: (protocols, { eq }) => eq(protocols.id, id),
+          with: {
+            author: true,
+            editor: true,
+          },
+        });
+      },
+      ['protocol', id],
+      {
+        tags: [`protocol-${id}`],
+      },
+    )();
+  } catch (error) {
+    console.error('Error fetching protocol by ID:', error);
+    return null;
+  }
 }
 
 export type ProtocolQueryResult = Awaited<ReturnType<typeof getProtocolById>>;
